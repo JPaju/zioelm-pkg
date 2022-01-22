2c6c6b8f50d5011406ceea6974ee46583b4f0a1b
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2022-01-22 18:56:33 +0200

    CORS to allow every origin and method

--- server/src/main/scala/Controller.scala
+++ server/src/main/scala/Controller.scala
@@ -3,10 +3,12 @@ import zio.json.*
 import zhttp.http.*
 
 object Controller:
+  val allowEverything = CORSConfig(anyOrigin = true, anyMethod = true)
+
   val httpApp: HttpApp[Has[PackageService], Nothing] = Http.collectZIO[Request] {
     case Method.GET -> !! / "packages"      => packageListing
     case Method.GET -> !! / "packages" / id => findPackageById(id)
-  }
+  } @@ Middleware.cors(allowEverything)
 
   private def packageListing =
     PackageService.getPackages.map { pkgs =>
